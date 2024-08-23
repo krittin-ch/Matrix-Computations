43c22d7ea65d9d9b82902bf6ee282c4ace1c7686
Krittin Chaowakarn <krittin.chao@gmail.com>
2024-08-23 11:04:52 +0700

    Create outer-product-update.jl

---
+++ 1-Matrix-Multiplication/1-Basics-Algorithms-and-Notation/outer-product-update.jl
@@ -0,0 +1,75 @@
+# Outer Product Update
+
+#=
+    A : Dimension (m, n)
+    x : Size (m,)
+    y : Size (n,)
+
+    Outer Product Update : A = A + x * transpose(y)
+=#
+
+x = Vector([1; 2; 3])
+y = Vector([4; 5])
+
+# println("Outer Product Output")
+# display(x * transpose(y))
+
+A = rand(-2:0.3:2, size(x, 1), size(y, 1))
+
+function outerProductUpdate(A::Matrix, x::Vector, y::Vector)
+    m, n = size(A)
+
+    res = deepcopy(A)
+
+    for i in 1:m
+        for j in 1:n
+            res[i, j] += x[i] * y[j]
+        end
+    end
+
+    return res
+end
+
+println("Original Matrix A : ")
+display(A)
+
+A1 = outerProductUpdate(A, x, y)
+
+println("Outer Product Updated Matrix A : ")
+display(A1)
+
+function outerProductUpdateWithRowOrientedSaxpy(A::Matrix, x::Vector, y::Vector)
+    m, n = size(A)
+
+    res = deepcopy(A)
+
+    for i in 1:m
+        res[i, :] += x[i] * y   
+        # should be `res[i, :] += x[i] * transpose(y)` since `y` is a column vector and res[i, :] is considered as a row vector
+        # but julia makes res[i, :] as a column vector
+    end
+
+    return res
+end
+
+A2 = outerProductUpdateWithRowOrientedSaxpy(A, x, y)
+
+println("Outer Product Updated Matrix A (Row-Oriented Saxpy) : ")
+display(A2)
+
+function outerProductUpdateWithColumnOrientedSaxpy(A::Matrix, x::Vector, y::Vector)
+    m, n = size(A)
+
+    res = deepcopy(A)
+
+    for j in 1:n
+        res[:, j] += y[j] * x
+    end
+
+    return res
+end
+
+A3 = outerProductUpdateWithColumnOrientedSaxpy(A, x, y)
+
+println("Outer Product Updated Matrix A (column-Oriented Saxpy) : ")
+display(A3)
